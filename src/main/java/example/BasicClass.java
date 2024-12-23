85ba4a0d3e6459251d086e9ba7a477e32d48a2ab
Ihor Taranenko <ihor.taranenko@Ihor.Taranenko>
2024-12-23 20:21:31 +0200

    1. Added dependency 'org.testng:testng:7.9.0' to build.gradle. 2. Created a simple test with setup and teardown methods. Each method has to output its name to console. 3. Created a test class named 'TestClassOne' with several test groups:     3.1 Created several groups of tests;     3.2 Group precondition and teardown methods, excluding one of them from test execution. 4. Created test class 'TestClassTwo' with 2 parametrised test methods:     4.1 The first method has to accept parameters from testng.xml;     4.2 The second method has to accept parameters from a dataprovider. 5. Created third test class named 'TestClassThree' where test methods are executed from the bottom to the top using priorities.

--- src/main/java/example/BasicClass.java
+++ src/main/java/example/BasicClass.java
@@ -1,7 +1,10 @@
 package example;
 
+import java.util.Arrays;
+
 public class BasicClass {
-    public static void main(String[] args) {
-        System.out.println("Hello Testng!");
+    public static int getSum(int[] numbers) {
+        return Arrays.stream(numbers).sum();
     }
+    //System.out.println("Hello from Testng!");
 }
